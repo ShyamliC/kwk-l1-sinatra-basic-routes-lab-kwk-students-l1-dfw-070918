c11a098b93eebe889216140227b96be5a604e3c1
ShyamliC <shyamli.channa@gmail.com>
2018-07-17 21:10:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,13 @@ class App < Sinatra::Base
   get '/' do
   "Hello, World!"
 end
+get '/name' do
+  "My name is Shyamli Channabasappa."
+end
+get '/hometown' do 
+  "My hometown is Frisco, Texas."
+end
+get '/favorite-song' do
+  "My favorite song"
+end
 end
